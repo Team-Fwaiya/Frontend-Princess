ce7abb7320f08efa3c0a08c3537c66f85403cab5
girin-sj <radianttwilight24@gmail.com>
2025-07-02 02:05:02 +0900

    first project setting

---
+++ princess/src/components/Window.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Window = () => {
+  return (
+    <div>
+      <h1>창입니다</h1>
+    </div>
+  );
+};
+
+export default Window;
---
+++ princess/src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./../../styles/Home/Home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>공주의 서재</h1>
+    </div>
+  );
+};
+
+export default Home;
